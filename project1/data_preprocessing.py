normalisation fixed + bias term

project1/data_preprocessing.py
@@ -131,7 +131,7 @@ def deal_with_specials(data_annotated, data):
 
 
 def normalize_data(data):
-    data = data / data.max(axis=0)
+    data = (data - data.min(axis=0)) / (data.max(axis=0) - data.min(axis=0))
     return data
 
 def one_hot_encode(data,data_annoted) : 
@@ -225,11 +225,13 @@ def preprocess_data2(x_train_raw, y_train, x_test_raw, annotated_data):
     x_train_filtered = remove_useless(x_train_raw, annotated_data)
     x_test_filtered = remove_useless(x_test_raw, annotated_data)
     data_train = clean_data(x_train_filtered, annotated_data)
-    data_test = clean_data(x_test_filtered, annotated_data)
+    x_test = clean_data(x_test_filtered, annotated_data)
     #np.savetxt('data_train.csv', data_train, delimiter=',')
     #np.savetxt('data_test.csv', data_test, delimiter=',')
     x_train, y_train = balance_data(y_train, data_train)
-    return x_train, y_train, data_test
+    x_train = np.hstack((np.ones((x_train.shape[0],1)), x_train))
+    x_test = np.hstack((np.ones((x_test.shape[0],1)), x_test))
+    return x_train, y_train, x_test
 
 def build_poly(x, degree, interactions=False):
     """